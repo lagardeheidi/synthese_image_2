fcb831fec75bd9b4d29ec6b52f623eceae6390cb
lagardeheidi <lagarde.heidi34@gmail.com>
2024-01-24 15:13:38 +0100

    TP3 disque

--- TP3_exo3_disque/main.cpp
+++ TP3_exo3_disque/main.cpp
@@ -1,40 +1,48 @@
+#include <cstddef>
+#include "glimac/default_shader.hpp"
+#include "glm/ext/scalar_constants.hpp"
 #include "glm/glm.hpp"
 #include "p6/p6.h"
+
 struct Vertex2DColor {
     glm::vec2 position;
     glm::vec3 color;
 };
+
+static constexpr int    N = 60;
+static constexpr double r = 0.5;
+
 int main()
 {
-    auto ctx = p6::Context{{1280, 720, "TP3 EX1"}};
+    auto ctx = p6::Context{{1280, 720, "TP3 EX5"}};
     ctx.maximize_window();
-    /*********************************
-     * HERE SHOULD COME THE INITIALIZATION CODE
-     *********************************/
 
+    // shader
     const p6::Shader shader = p6::load_shader(
         "shaders/triangle.vs.glsl",
         "shaders/triangle.fs.glsl"
     );
-    // Création d'un seul vbo
+
+    // INITIALISATION
+
+    // création de vbo (ici un seul) = tableau permettant de stocker données d'un élément, puis binding
     GLuint vbo;
     glGenBuffers(1, &vbo);
-    // À partir de ce point, la variable vbo contient l'identifiant d'un VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); // binding
 
-    // binding d'un VBO sur la cible GL_ARRAY_BUFFER
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // On peut à prénset modifier le VBO en passant par la cible GL_ARRAY_BUFFER
-    // creer un tableau de GLfloat contenant toutes les coordonnées à la suite
-    Vertex2DColor vertices[] = {
-        Vertex2DColor{{-0.5f, -0.5f}, {1.f, 0.f, 0.f}}, // Premier sommet
-        Vertex2DColor{{0.5f, -0.5f}, {0.f, 1.f, 0.f}},  // Deuxième sommet
-        Vertex2DColor{{0.5f, 0.5f}, {0.f, 0.f, 1.f}},   // Troisième sommet
-        Vertex2DColor{{-0.5f, -0.5f}, {1.f, 0.f, 0.f}}, // Quatrième sommet
-        Vertex2DColor{{-0.5f, 0.5f}, {0.f, 1.f, 0.f}},  // Cinquième sommet
-        Vertex2DColor{{0.5f, 0.5f}, {0.f, 0.f, 1.f}}    // Sixième sommet
-    };
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(Vertex2DColor), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Debinder VBO
+    // on créé un vecteur avec les coordonnées du triangle
+    std::vector<Vertex2DColor> vertices;
+    for (int i = 0; i < N; i++)
+    {
+        vertices.push_back(Vertex2DColor{{0.f, 0.f}, {1.f, 1.f, 0.2f}});
+        vertices.push_back(Vertex2DColor{{r * glm::cos(2 * glm::pi<float>() / N * i), r * glm::sin(2 * glm::pi<float>() / N * i)}, {0.8f, 0.2f, 0.8f}});
+        vertices.push_back(Vertex2DColor{{r * glm::cos(2 * glm::pi<float>() / N * (i + 1)), r * glm::sin(2 * glm::pi<float>() / N * (i + 1))}, {0.f, 0.7f, 0.9f}});
+    }
+    // puis on envoie les données
+    glBufferData(GL_ARRAY_BUFFER, 3 * N * sizeof(Vertex2DColor), data(vertices), GL_STATIC_DRAW);
+
+    // on debind pour éviter de modifier le vbo par erreur, meme fonction qu'au début mais avec 0
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // creation d'un vao
     GLuint vao;
@@ -82,16 +90,13 @@ int main()
 
         // Dessiner en utilisant le VAO
         //(le type de primitive, l'indice du premier sommet, le nb de sommets)
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        // debindez le vao
-        glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 3 * N);
+        glBindVertexArray(0); // debind du vao
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
-    // liberer les ressources allouees sur GPU
-    // desallouer le vbo et vao
     glDeleteBuffers(1, &vbo);
     glDeleteVertexArrays(1, &vao);
+    return 0;
 }
